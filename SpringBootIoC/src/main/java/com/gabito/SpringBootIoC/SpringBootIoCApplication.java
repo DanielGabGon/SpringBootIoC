8ae0f6f31118dd1b1e678c93d7b0b330ed2b4bdc
DanielGabGon <dan.gabito@gmail.com>
2024-07-19 10:22:05 -0600

    Logro: Poner en práctica el uso de los scopes/ambitos de los beans de spring creando un carrito de compras.

--- SpringBootIoC/src/main/java/com/gabito/SpringBootIoC/SpringBootIoCApplication.java
+++ SpringBootIoC/src/main/java/com/gabito/SpringBootIoC/SpringBootIoCApplication.java
@@ -1,6 +1,8 @@
 package com.gabito.SpringBootIoC;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabito.SpringBootIoC.service.ProductService;
+import com.gabito.SpringBootIoC.service.ShoppingCarService;
 
 @SpringBootApplication
 @RestController
@@ -18,6 +21,9 @@ public class SpringBootIoCApplication{
 
     @Autowired
 	private ProductService  productService;
+    
+    @Autowired
+    private ShoppingCarService shoppingCarService;
 	
 	
 
@@ -35,6 +41,21 @@ public class SpringBootIoCApplication{
 		productService.save("Zapatos Nike");
 		return "Hello Spring Boot IoC";
 	}
+	
+	@RequestMapping("/add-products")
+	public String addProduct(){
+		shoppingCarService.add();
+		return "Producto agregado";
+	}
+	
+	@RequestMapping("/get-products")
+	public List<String>  getProduct(){
+		return shoppingCarService.get();
+	}
+	
+	
+	
+	
 	
 
 }
